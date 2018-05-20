a0179e4fbaf8c3598d0e18461bf8c8bc6043c811
Jake <jacobelong@gmail.com>
2018-05-19 23:07:53 -0400

    About section, adding stuff to things and stuff

--- src/components/ButtonLink.js
+++ src/components/ButtonLink.js
@@ -1,18 +1,26 @@
 import React from "react";
+import FontAwesomeIcon from '@fortawesome/react-fontawesome'
+import {faLongArrowAltRight} from '@fortawesome/fontawesome-free-solid'
 
 class ButtonLink extends React.Component {
   render() {
     const classes = `button-link ${this.props.size} ${this.props.type}`;
 
     return (
-      <a href={this.props.href} className={classes}>{this.props.text}</a>
+      <a href={this.props.href} className={classes}>
+        {this.props.text}
+        {this.props.hasArrow ? (
+          <FontAwesomeIcon icon="long-arrow-alt-right" style={{marginLeft: '1em'}} />
+        ) : ('')
+        }
+      </a>
     );
   }
 }
 
 ButtonLink.defaultProps = {
   size: 'regular',
-  type: 'primary'
+  type: 'primary',
 };
 
 export default ButtonLink;
--- src/pages/index.js
+++ src/pages/index.js
@@ -87,6 +87,57 @@ class Index extends React.Component {
           </ServicesWrapper>
         </section>
 
+        <section className="about">
+          <ButtonLink
+            href="/contact"
+            size="small"
+            type="primary"
+            text="Explore our work"
+            hasArrow
+          />
+          <div className="content">
+            <header>
+              <h1>About Might & Method</h1>
+              <p>In a nutshell of sorts, <span>Might & Method</span> is a digital agency, but we prefer to think of ourselves as digital alchemists. We convert pixels into magic for you and your customers. The secret’s in the sauce, baby.</p>
+            </header>
+
+            <h2>How We Work</h2>
+            <p>Our ultimate goal is to create something that meets your needs while delighting your customers. If we were to put a Venn diagram of that statement here, it would look something like two slightly different-colored circles — one labeled “Meets your needs”, the other labeled “Delights your customers” — overlapping in the middle, and it’d say “The Sweet Spot” or something like that. But you get the idea. Unless you’ve never seen a Venn diagram, in which case, congrats.</p>
+
+            <div className="grid">
+              <div className="row">
+                <div>
+                  <h3>Getting to know you & your company</h3>
+                  <p>Every final product should be a truthful reflection of your business, so we dig into your goals, processes, resources, opportunities and limitations. Think of us as the tailor taking every measurement on your new pair of silky smooth pants before you wear them in public. Looking good.</p>
+                </div>
+
+                <div>
+                  <h3>Understanding your customers</h3>
+                  <p>Sure, we said "audience" up there, but we're talking about people, people! We help you understand the distinct individuals who make up your target audience so you can discover how to guide them from "maybe" to "yes"—to "yes" yet again.</p>
+                </div>
+              </div>
+
+              <div className="row">
+                <div>
+                  <h3>Defining opportunities for growth</h3>
+                  <p>We're committed to giving you exactly what you want, but more than that, we aim to define goals first and determine the best course of action to meet them. We identify where potential solutions overlap with customers' dilemmas to drive a winning strategy.</p>
+                </div>
+
+                <div>
+                  <h3>Build the dang thing&trade;</h3>
+                  <p>We define a clear process, identify specific needs for execution and set a step-by-step timeline. Those sound a little rigid, we know, but we also know that steps in a process can pivot, needs can change and key dates move. We execute because we adapt. We're limber folks—for the most part.</p>
+                </div>
+              </div>
+            </div> {/* /grid */}
+
+            <div style={{ width: '100%', textAlign: 'center' }}>
+              <Link to="/services">
+                See how we can help your company <FontAwesomeIcon icon={faArrowDown} />
+              </Link>
+            </div>
+          </div>
+        </section>
+
         <WorkSamples>
           <Work
             category="ui/ux design"
