b796ae92fab75b26550bd53f31c9d88aa245ac22
Jake <jacobelong@gmail.com>
2018-05-22 21:58:46 -0400

    Mobile nav progress

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -18,45 +18,42 @@ class Nav extends React.Component {
 
   render() {
     return (
-      <div>
-        <nav id="nav" className={this.props.sticky ? 'sticky' : ''}>
-          <Link to="/" style={{ lineHeight: 1 }}>
-            <img
-              src={logo}
-              alt="Might & Method"
-              style={{
-                width: '40px'
-              }}
-            />
-          </Link>
+      <nav id="nav" className={this.props.sticky ? 'sticky' : ''}>
+        <Link to="/" style={{ lineHeight: 1 }} className="logo">
+          <img
+            src={logo}
+            alt="Might & Method"
+            style={{
+              width: '40px'
+            }}
+          />
+        </Link>
 
-          <ul className="menu">
-            <li>
-              <Link to="/services">What we do</Link>
-            </li>
-            <li>
-              <Link to="/work">Our work</Link>
-            </li>
-            <li>
-              <Link to="/pricing">Pricing</Link>
-            </li>
-            <li>
-              <Link to="/about">About</Link>
-            </li>
-            <li>
-              <Link to="/contact">Contact</Link>
-            </li>
-          </ul>
-          
-          <a onClick={this.toggleSidenav.bind(this)} className={`mobile-menu ${this.state.showHideMobileNav}`}>
-            <ul className="buns">
-              <li className="bun" />
-              <li className="bun" />
-            </ul> 
-          </a>
-        </nav>
-        {/* <Sidenav className={this.props.showHideSidenav} /> */}
-      </div>
+        <ul className={`menu ${this.state.showHideMobileNav}`}>
+          <li>
+            <Link to="/services">What we do</Link>
+          </li>
+          <li>
+            <Link to="/work">Our work</Link>
+          </li>
+          <li>
+            <Link to="/pricing">Pricing</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact</Link>
+          </li>
+        </ul>
+        
+        <a onClick={this.toggleSidenav.bind(this)} className={`mobile-menu ${this.state.showHideMobileNav}`}>
+          <ul className="buns">
+            <li className="bun" />
+            <li className="bun" />
+          </ul> 
+        </a>
+      </nav>
     );
   }
 }
